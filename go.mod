c6b6a9833d9618c61da57cc6bae90e01527c4f54
sodai.hirai <sodai.hirai@enigmo.co.jp>
2024-04-07 16:44:28 +0900

    init service

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.9.1
 	golang.org/dl v0.0.0-20240305174203-2885f567d808 // indirect
+	golang.org/x/oauth2 v0.19.0
+	google.golang.org/api v0.172.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 import (
+    "context"
+    "os"
     "net/http"
+    "log"
     "github.com/gin-gonic/gin"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/google"
+    "google.golang.org/api/drive/v3"
+    "google.golang.org/api/option"
 )
 
 func main() {
@@ -13,4 +20,39 @@ func main() {
 
 func upload(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, "upload starting")
+}
+
+func getDriveService() *drive.Service {
+    ctx := context.Background()
+    b, err := os.ReadFile("credentials.json")
+    if err != nil {
+            log.Fatalf("Unable to read client secret file: %v", err)
+    }
+
+    // If modifying these scopes, delete your previously saved token.json.
+    config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/drive.install")
+    if err != nil {
+            log.Fatalf("Unable to parse client secret file to config: %v", err)
+    }
+    client := getClient(config)
+
+    srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+    if err != nil {
+            log.Fatalf("Unable to retrieve Drive client: %v", err)
+    }
+
+    return srv
+}
+
+func getClient(config *oauth2.Config) *http.Client {
+    tokFile := "token.json"
+    tok := tokenFromFile(tokFile)
+    return config.Client(context.Background(), tok)
+}
+
+func tokenFromFile(file string) (*oauth2.Token) {
+    f, _ := os.Open(file)
+    defer f.Close()
+    tok := &oauth2.Token{}
+    return tok
 }
